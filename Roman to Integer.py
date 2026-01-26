e63ebb7fee7e0ab1daddf4c58825bf6a939b1f89
Omar Faruk Fahim <omarfaruqfahim007@gmail.com>
2026-01-26 23:32:48 +0600

    Roman to Integer

---
+++ Roman to Integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n - 1):
+            current_val = roman_map[s[i]]
+            next_val = roman_map[s[i+1]]
+            
+            if current_val < next_val:
+                total -= current_val
+            else:
+                total += current_val
+                
+        total += roman_map[s[-1]]
+        
+        return total
