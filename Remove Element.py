781a88c28b58e84ec9ea95f74452456896addbc3
Omar Faruk Fahim <omarfaruqfahim007@gmail.com>
2026-01-31 23:35:49 +0600

    new

---
+++ Remove Element.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        writer = 0
+        
+        for reader in range(len(nums)):
+            if nums[reader] != val:
+                nums[writer] = nums[reader]
+                
+                writer += 1
+                
+        return writer
